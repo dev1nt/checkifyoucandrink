4075ce805a88485be455f4a6c37291c8a42c2ae4
dev1nt <devinrazer42@gmail.com>
2022-04-01 22:02:51 -0400

    added functions

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+// define function called canYouDrink
+function canYouDrink () {
 // define age variable and sends a prompt with a question to the user. 
-var age = prompt("What is your age?");
+    var age = prompt("What is your age?");
 // if statement to check if the users age is 21.
-if (Number(age) === 21){
+    if (Number(age) === 21){
 // this alert will give a response to the user telling them that they're now eligible to drink.
-    alert("You're Now Legal to drink anywhere.");
+        alert("You're" + " NOW " + "LEGAL to drink anywhere.");
 // this else if checks if user is less than 21.
-} else if (Number(age) < 21) {
+    } else if (Number(age) < 21) {
 // this alert will give a response to the user telling them that they're not eligible to drink.
-    alert("You're not legal to drink anywhere.");
+        alert("You're" + " NOT " + "LEGAL to drink in the U.S.");
 // this will close your window after you hit ok.
-    window.close(); 
+        window.close();
 // this else if checks if user is over 21.
-} else if (Number(age) > 21) {
+    } else if (Number(age) > 21) {
 // this alert will give a response to the user telling them that they're eligible to drink.
-    alert("You're legal to drink anywhere.");
-} 
+        alert("You're" + " LEGAL " + "to drink anywhere.")
+    }
+}
+canYouDrink();
